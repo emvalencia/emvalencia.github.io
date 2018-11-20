27714136e7d27c91248b831c6b588f795bf20b4a
Emery Valencia <emeryvalencia@gmail.com>
2018-11-20 11:15:20 -0800

    Updates

---
+++ precache-manifest.38cbd8453c7b3bcf34caa2246799e8dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a74baa55d45063e46417bad8491a660a",
+    "url": "/static/media/mountain.a74baa55.jpg"
+  },
+  {
+    "revision": "16b07a03d7ad662a387fee29bed1d178",
+    "url": "/static/media/forest.16b07a03.jpg"
+  },
+  {
+    "revision": "26eed4c3503b8de57efb7d342e62579e",
+    "url": "/static/media/carefree.26eed4c3.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ec861eaff4082102fc9",
+    "url": "/static/js/main.6ec861ea.chunk.js"
+  },
+  {
+    "revision": "a08a861a0abe2e1d2292",
+    "url": "/static/js/1.a08a861a.chunk.js"
+  },
+  {
+    "revision": "6ec861eaff4082102fc9",
+    "url": "/static/css/main.60967814.chunk.css"
+  },
+  {
+    "revision": "ed09863f7280fdb5e998eb16a6baa9e7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.268ca7eee6d23c18c4d0a84e196b3f46.js"
+  "/precache-manifest.38cbd8453c7b3bcf34caa2246799e8dd.js"
 );
 
 workbox.clientsClaim();
