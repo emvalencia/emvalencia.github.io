48f73b5c7de0c4a094a4b44b3c735561b04527ec
Emery Valencia <emeryvalencia@gmail.com>
2018-11-20 08:53:12 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de10f9a44e5e5eec11f91ff4b6f6e5ae.js"
+  "/precache-manifest.268ca7eee6d23c18c4d0a84e196b3f46.js"
 );
 
 workbox.clientsClaim();
